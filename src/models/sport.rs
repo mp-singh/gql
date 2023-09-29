412b7d03c7073a683632e36ed3912717ad1ea12c
Prince <mandeep@mandeepsingh.ca>
2023-09-29 22:09:12 +0000

    cleanup

--- src/models/sport.rs
+++ src/models/sport.rs
@@ -5,13 +5,3 @@ pub struct Sport {
     pub id: i32,
     pub name: String,
 }
-
-// #[graphql_object(context = Database)]
-// impl Sport {
-//     //  your resolvers
-
-//     // To call the dataloader
-//     async fn get_sport_by_ids(ctx: &Database, id: i32) -> Sport {
-//         ctx.sport_loader.load(id).await
-//     }
-// }
